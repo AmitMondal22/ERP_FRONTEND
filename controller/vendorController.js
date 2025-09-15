533cde1e320fb0d9177a29299622dca2fbf01e75
Amit Mondal <amit.ndk3@gmail.com>
2025-09-15 19:07:50 +0530

    all vendor and contact_person add list edit and delete

--- controller/vendorController.js
+++ controller/vendorController.js
@@ -15,14 +15,14 @@ class VendorController {
   // Add Vendor
   addVendor = async (req, res) => {
     try {
-      const { name, mobile, email, city_id, address, gst_in } = req.body;
+      const { name, mobile, email, city_id, address, gst_in, contactPerson } = req.body;
       const created_at = dayjs().utc().format("YYYY-MM-DD HH:mm:ss");
 
       const columns = [
         "vendor_name",
         "vendor_mobile",
         "vendor_email",
-        "vendor_city_id",
+        "city_id",
         "vendor_address",
         "vendor_gst_in",
         "created_by",
@@ -35,6 +35,22 @@ class VendorController {
       // insertData(tableName, columnsArray, valuesArray)
       const vendor_id = await insertData("md_vendor", columns, values);
 
+        for await (const contact of data.contactPerson) {
+          let c_columns = [
+              'contact_person_name',
+              'contact_person_email',
+              'contact_person_mobile_no',
+              'contact_person_remarks',
+              'fatch_id',
+              'type',
+              'created_by',
+              'created_at'
+            ]
+          ,c_values =[contact.name, contact.email, contact.mobile, "", vendor_id, 'VN', 1, created_at]
+          await insertData("md_contact_person", c_columns, c_values);
+        }
+
+
       res.status(201).json({
         success: true,
         message: "Vendor created successfully",
@@ -46,7 +62,7 @@ class VendorController {
           city_id,
           address,
           gst_in,
-          created_by,
+          created_by:1,
           created_at,
         },
       });
@@ -59,14 +75,19 @@ class VendorController {
   // Get all vendors
   getAllVendors = async (req, res) => {
     try {
-      const vendors = await selectData("md_vendor", "*");
-      if (!vendors || vendors.length === 0) {
-        return res.status(404).json({ success: false, message: "No vendors found" });
-      }
-      res.status(200).json({ success: true, data: vendors });
+        const table = "md_vendor as a, lo_cities as b, lo_states as c";
+        const condition = `a.city_id = b.id AND b.state_id = c.id`;
+        // Give unique aliases for duplicate column names
+        const select = "a.*, b.name AS city_name, b.state_id, c.name AS state_name";
+
+        const vendors = await selectData(table, select, condition,"a.vendor_name ASC");
+        if (!vendors || vendors.length === 0) {
+          return res.status(404).json({ success: false, message: "No vendors found" });
+        }
+        res.status(200).json({ success: true, data: vendors });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ success: false, message: "Unable to fetch vendors" });
+        console.error(error);
+        res.status(500).json({ success: false, message: "Unable to fetch vendors" });
     }
   };
 
@@ -74,11 +95,20 @@ class VendorController {
   getVendorById = async (req, res) => {
     try {
       const { vendor_id} = req.params;
-      const vendor = await selectOneData("md_vendor", "*", `id = ${vendor_id}`);
-      if (!vendor) {
+      const table = "md_vendor as a, lo_cities as b, lo_states as c";
+      const condition = `a.city_id = b.id AND b.state_id = c.id AND id = ${vendor_id}`;
+      // Give unique aliases for duplicate column names
+      const select = "a.*, b.name AS city_name, b.state_id, c.name AS state_name";
+      const vendor = await selectOneData(table, select, condition);
+       if (!vendor) {
         return res.status(404).json({ success: false, message: "Vendor not found" });
       }
-      res.status(200).json({ success: true, data: vendor });
+
+      const contact_person = await selectOneData("md_contact_person", "*", `fatch_id = ${vendor_id}`);
+      if (!contact_person) {
+        return res.status(404).json({ success: false, message: "Contact Person not found" });
+      }
+      res.status(200).json({ success: true, data: {vendor, contact_person} });
     } catch (error) {
       console.error(error);
       res.status(500).json({ success: false, message: "Unable to fetch vendor" });
@@ -89,19 +119,55 @@ class VendorController {
   updateVendor = async (req, res) => {
     try {
       const { id } = req.params;
-      const updateFields = req.body;
-      console.log(">>>>>>>>>>>>")
+      const { name, mobile, email, city_id, address, gst_in } = req.body;
+      const updated_at = dayjs().utc().format("YYYY-MM-DD HH:mm:ss");
+      const updateValues = {
+        vendor_name:name,
+        vendor_mobile:mobile,
+        vendor_email:email,
+        city_id,
+        vendor_address:address,
+        vendor_gst_in:gst_in,
+        updated_at,
+      };
 
-      const affectedRows = await updateData("md_vendor", updateFields, `id = ${id}`);
+      const affectedRows = await updateData("md_vendor", updateValues, `vendor_id = ${id}`);
       if (affectedRows === 0) {
         return res.status(404).json({ success: false, message: "Vendor not found or no changes made" });
       }
 
-      const updatedVendor = await selectOneData("md_vendor", "*", `id = ${id}`);
-      res.status(200).json({ success: true, message: "Vendor updated successfully", data: updatedVendor });
+      
+      res.status(200).json({ success: true, message: "Vendor updated successfully", data: affectedRows });
     } catch (error) {
       console.error(error);
       res.status(500).json({ success: false, message: "Unable to update vendor" });
+    }
+  };
+
+
+
+
+  updateContactPerson = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, mobile, email } = req.body;
+      const updated_at = dayjs().utc().format("YYYY-MM-DD HH:mm:ss");
+      const updateValues = {
+        contact_person_name:name,
+        contact_person_email:email,
+        contact_person_mobile_no:mobile,
+        contact_person_remarks:"",
+        updated_at
+      };
+
+      const affectedRows = await updateData("md_contact_person", updateValues, `vendor_id = ${id}`);
+      if (affectedRows === 0) {
+        return res.status(404).json({ success: false, message: "Contact Person not found or no changes made" });
+      }
+      res.status(200).json({ success: true, message: "Contact Person updated successfully", data: affectedRows });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: "Contact Person Unable to update vendor" });
     }
   };
 
@@ -113,12 +179,32 @@ class VendorController {
       if (deletedRows === 0) {
         return res.status(404).json({ success: false, message: "Vendor not found" });
       }
-      res.status(200).json({ success: true, message: "Vendor deleted successfully" });
+      const deletedContactPersonRows = await deleteData("md_contact_person", `fatch_id = ${id} AND type = 'VN'`);
+      if (deletedContactPersonRows === 0) {
+        return res.status(404).json({ success: false, message: "Contact Person not found" });
+      }
+      res.status(200).json({ success: true, message: "Vendor and Contact Person deleted successfully" });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ success: false, message: "Unable to delete vendor" });
+      res.status(500).json({ success: false, message: "Unable to delete vendor and Contact Person" });
     }
   };
+
+
+  deleteContactPerson = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deletedContactPersonRows = await deleteData("md_contact_person", `contact_person_id  = ${id}`);
+      if (deletedContactPersonRows === 0) {
+        return res.status(404).json({ success: false, message: "Contact Person not found" });
+      }
+      res.status(200).json({ success: true, message: "Contact Person deleted successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: "Unable to delete Contact Person" });
+    }
+  };
+
 }
 
 module.exports = new VendorController();
--- router/vendorRoutes.js
+++ router/vendorRoutes.js
@@ -9,6 +9,8 @@ router.post("/api/createvendor", vendorController.addVendor);
 router.get("/api/getallvendor", vendorController.getAllVendors);
 router.get("/api/getvendor/:id", vendorController.getVendorById);
 router.put("/api/vendor/:id", vendorController.updateVendor);
+router.put("/api/contact_person/:id", vendorController.updateContactPerson);
 router.delete("/api/vendor/:id", vendorController.deleteVendor);
+router.delete("/api/contact_person/:id", vendorController.deleteContactPerson);
 
 module.exports = router;
