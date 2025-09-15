318502fa1e8b5b74667cf717d6be9d17248bc171
Amit Mondal <amit.ndk3@gmail.com>
2025-09-15 19:21:30 +0530

    change method

--- router/vendorRoutes.js
+++ router/vendorRoutes.js
@@ -9,8 +9,8 @@ router.post("/api/createvendor", vendorController.addVendor);
 router.post("/api/create_contact_person", vendorController.addContactPerson);
 router.get("/api/getallvendor", vendorController.getAllVendors);
 router.get("/api/getvendor/:id", vendorController.getVendorById);
-router.put("/api/vendor/:id", vendorController.updateVendor);
-router.put("/api/contact_person/:id", vendorController.updateContactPerson);
+router.post("/api/vendor/:id", vendorController.updateVendor);
+router.post("/api/contact_person/:id", vendorController.updateContactPerson);
 router.delete("/api/vendor/:id", vendorController.deleteVendor);
 router.delete("/api/contact_person/:id", vendorController.deleteContactPerson);
 
