27dfc230adcf01895f8ba86abdfde4b7d266b742
Amit Mondal <amit.ndk3@gmail.com>
2025-09-16 14:34:35 +0530

    add get employee_id to employee data

--- router/employeeRoutes.js
+++ router/employeeRoutes.js
@@ -8,7 +8,7 @@ const authcheck= require('../middleware/auth')
 router.post("/api/add_employee",authcheck, employeeController.addEmployee);
 
 router.get("/api/get_all_employee",authcheck, employeeController.getAllemployee);
-// router.get("/api/getvendor/:id",authcheck, vendorController.getVendorById);
+router.get("/api/get_employee/:employee_id",authcheck, employeeController.getEmployeeById);
 // router.post("/api/vendor/:id",authcheck, vendorController.updateVendor);
 // router.post("/api/contact_person/:id",authcheck, vendorController.updateContactPerson);
 // router.delete("/api/vendor/:id",authcheck, vendorController.deleteVendor);
