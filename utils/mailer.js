3fd69db2f9fe6a976ade9ab3515e79aad4c77f96
sourish chatterjee <chatterjeesourishchatterjee@gmail.com>
2026-07-09 13:08:03 +0530

    test++

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,14 +1,14 @@
-// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 // const sendEmployeeCredentials = async ({ to, employeeId, password }) => {
 //   if (!to) return;
